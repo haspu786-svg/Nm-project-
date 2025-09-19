ffcc9ef8e7ba1daaee8b4509020e3a711dad0678
haspu786-svg <haspu786@gmail.com>
2025-09-19 12:12:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,19 @@
-const darkToggle = document.getElementById('darkModeToggle');
-
-const body = document.body;
-
-darkToggle.addEventListener('click', () => {
-
-  body.classList.toggle('dark-mode');
-
-  if (body.classList.contains('dark-mode')) {
-
-    localStorage.setItem('theme', 'dark');
-
-  } else {
-
-    localStorage.setItem('theme', 'light');
-
-  }
-
+function toggleMenu(){
+document.querySelector(".nav-links").classList.toggle("show");
+}
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("successMsg").style.display = "block";
+    this.reset();
 });
+function loadProject(url) {
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("viewer").style.display = "block";
+    document.getElementById("projectFrame").src = url;
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  const savedTheme = localStorage.getItem('theme');
-
-  if(savedTheme === 'dark') {
-
-    body.classList.add('dark-mode');
-
-  }
-
-});
-
-const form = document.getElementById('contact-form');
-
-const formMessage = document.getElementById('form-message');
-
-form.addEventListener('submit', (e) => {
-
-  e.preventDefault();
-
-  const name = form.name.value.trim();
-
-  const email = form.email.value.trim();
-
-  const message = form.message.value.trim();
-
-  if(name.length < 2) {
-
-    formMessage.style.color = 'red';
-
-    formMessage.textContent = 'Name must be at least 2 characters.';
-
-    return;
-
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if(!emailRegex.test(email)) {
-
-    formMessage.style.color = 'red';
-
-    formMessage.textContent = 'Please enter a valid email.';
-
-    return;
-
-  }
-
-  if(message.length < 10) {
-
-    formMessage.style.color = 'red';
-
-    formMessage.textContent = 'Message must be at least 10 characters.';
-
-    return;
-
-  }
-
-  formMessage.style.color = 'green';
-
-  formMessage.textContent = 'Message sent successfully! Thank you.';
-
-  form.reset();
-
-})
+function closeViewer() {
+    document.getElementById("viewer").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+    document.getElementById("projectFrame").src = "";
+}
